1657ce29e8416fbd5dace3c8c4d88edb0e448c6d
toni123654123 <tn1061999@gmail.com>
2020-12-17 11:17:47 +0700

    chuan chi

--- src/main/java/com/codegym/demo/service/CityServiceImpl.java
+++ src/main/java/com/codegym/demo/service/CityServiceImpl.java
@@ -11,7 +11,7 @@ import org.springframework.stereotype.Service;
 import java.util.Optional;
 
 @Service
-public class CityServiceIPL implements ICityService{
+public class CityServiceImpl implements ICityService{
 
     @Autowired
     private ICityRepository iCityRepository;
